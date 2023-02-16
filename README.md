bcc91d64dc764d4839f4b40a14733c4f85552a0e
sinhaGuild <anurag.r.sinha@gmail.com>
2023-02-16 16:56:27 -0500

    jinja2, tailwindcss configured.

--- README.md
+++ README.md
@@ -1,22 +1,53 @@
-# Poetry Workflow
+##### FASTAPI - POETRY - PYDANTIC - SQLALCHEMY - JINJA2 - TAILWINDCSS - SQLITE
 
-## 1. Clone
+# FastAPI Microservice boilerplate
 
+This is an opinionated boilerplate to help bootstrap complex, large scale ML endpoints (or any kind of multiroute project).
+
+### Features
+
+- `fastapi.Router` External Routing pattern `src/api/routes`
+- `Poetry` for dependency management
+- `Poetry run dev` development and build scripts
+- `Pydantic` for data modelling
+- `sqlalchemy` for schema modelling (ORM) and crud.
+- `sqlite` database
+- `src/api/static` static file serving (images)
+- `jinja2` templates for html file service (homepage)
+- `tailwindcss` for template styling
+
+Roadmap:
+
+- `bentoml` for model serving
+- `mlflow` for model devops
+- `az` azure ml studio for end point deployment
+
+---
+
+### How to use
+
+#### 1. Clone
 
 ```zsh
 git clone ... <project_name> && cd <project_name>
 ```
 
-## 2. Installing dependencies
+#### 2. Installing dependencies
 
 ```sh
 poetry add `cat requirements.txt`
 ```
 
-## 3. Running the server
-
+#### 3. Running the server
 
 ```sh
 poetry run dev
 ```
 
+#### 4. Navigate
+
+##### `Home` - \
+
+##### `Swagger` - \docs
+
+##### `Redoc` - \redoc
--- src/api/main.py
+++ src/api/main.py
@@ -1,30 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .routers import books
 import uvicorn
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+# instantiate app
+app = FastAPI(
+    swagger_ui_parameters={
+        "syntaxHighlight.theme": "arta",
+        "displayRequestDuration": True,
+        "showCommonExtensions": True,
+        "tryItOutEnabled": True,
+        "deepLinking": True,
+
+        }
+)
+
+# mount static files and templates
+app.mount("/static", StaticFiles(directory="static"), name='static')
+templates = Jinja2Templates(directory="templates")
+
+#   Home page
+@app.get("/", response_class=HTMLResponse)
+async def home_page(request: Request):
+    return templates.TemplateResponse("homepage.html", {"request": request})
+
 
 # include routes
 app.include_router(
     books.router,
     prefix="/books",
-    tags=["books"],
+    tags=["Books"],
     responses={404: {"description": "Not found!"}, 418: {"description": "I'm a teapot!"}},
 )
 
 
-# scripts for poetry
+# scripts for poetry: development
 def dev():
     uvicorn.run(
-        "src.api.main:app", 
+        "api.main:app", 
         host="0.0.0.0", 
         port=8000, 
         reload=True
     )
 
+# scripts for poetry: production
 def start():
     uvicorn.run(
-        "src.api.main:app", 
+        "api.main:app", 
         host="0.0.0.0", 
         port=8000, 
         reload=False
